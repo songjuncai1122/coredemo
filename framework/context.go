2e71f598cd150957e56bada546c0fb29daa1b425
songjuncai <czsongjuncai@163.com>
2022-09-01 17:29:01 +0800

    第二天第一次提交

---
+++ framework/context.go
@@ -0,0 +1,76 @@
+package framework
+
+import (
+	"context"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type Context struct {
+	request        *http.Request
+	responseWriter http.ResponseWriter
+	ctx            context.Context
+	handler        ControllerHandler
+
+	// 是否超时标记位
+	hasTimeout bool
+	// 写保护机制
+	writeMux *sync.Mutex
+}
+
+// #region baes function
+
+func NewContext(r *http.Request, w http.ResponseWriter) *Context {
+	return &Context{
+		request:        r,
+		responseWriter: w,
+		ctx:            r.Context(),
+		writeMux:       &sync.Mutex{},
+	}
+}
+
+func (ctx *Context) WriterMux() *sync.Mutex {
+	return ctx.writeMux
+}
+
+func (ctx *Context) GetRequest() *http.Request {
+	return ctx.request
+}
+
+func (ctx *Context) GetResponse() http.ResponseWriter {
+	return ctx.responseWriter
+}
+
+func (ctx *Context) SetHasTimeout() {
+	ctx.hasTimeout = true
+}
+
+func (ctx *Context) HasTimeout() bool {
+	return ctx.hasTimeout
+}
+
+// #endregion
+
+func (ctx *Context) BaseContext() context.Context {
+	return ctx.request.Context()
+}
+
+// #region implement context.Context
+func (ctx *Context) Deadline() (deadline time.Time, ok bool) {
+	return ctx.BaseContext().Deadline()
+}
+
+func (ctx *Context) Done() <-chan struct{} {
+	return ctx.BaseContext().Done()
+}
+
+func (ctx *Context) Err() error {
+	return ctx.BaseContext().Err()
+}
+
+func (ctx *Context) Value(key interface{}) interface{} {
+	return ctx.BaseContext().Value(key)
+}
+
+// #endregion
