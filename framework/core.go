7b4e01f17a29e72bc95815e73c49d2c4c04bd5c9
songjuncai <czsongjuncai@163.com>
2022-09-05 22:05:02 +0800

    第二天第二次提交

--- framework/core.go
+++ framework/core.go
@@ -1,14 +1,32 @@
 package framework
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
+// Core represent core struct
 type Core struct {
+	router map[string]ControllerHandler
 }
 
 func NewCore() *Core {
-	return &Core{}
+	return &Core{router: map[string]ControllerHandler{}}
+}
+
+func (c *Core) Get(url string, handler ControllerHandler) {
+	c.router[url] = handler
 }
 
 func (c *Core) ServeHTTP(response http.ResponseWriter, request *http.Request) {
-	// TODO
+	log.Println("core.serverHTTP")
+	ctx := NewContext(request, response)
+
+	// 一个简单的路由选择器，这里直接写死为测试路由foo
+	router := c.router["foo"]
+	if router == nil {
+		return
+	}
+	log.Println("core.router")
+	router(ctx)
 }
